3279842ba0bc55adb6671fcdd214ffcc7d86d6ab
brunogomes98 <brunog.vix@gmail.com>
2022-04-09 12:36:29 -0300

    string

--- ruby-full-stack/ruby-basic-data-types/string.rb
+++ ruby-full-stack/ruby-basic-data-types/string.rb
@@ -39,3 +39,61 @@ puts 'Hello, #{name}' #=> "Hello, #{name}"
 # Caps
 
 "hello".capitalize #=> "Hello"
+
+
+# Include 
+"hello".include?("lo")  #=> true
+
+"hello".include?("z")   #=> false
+
+
+#upcase
+
+"hello".upcase  #=> "HELLO"
+
+
+#downcase
+
+"Hello".downcase  #=> "hello"
+
+
+#empty?
+
+"hello".empty?  #=> false
+
+"".empty?       #=> true
+
+
+#length
+
+"hello".length  #=> 5
+
+
+#reverse
+
+"hello".reverse  #=> "olleh"
+
+
+#split
+
+"hello world".split  #=> ["hello", "world"]
+
+"hello".split("")    #=> ["h", "e", "l", "l", "o"]
+
+
+#strip
+
+" hello, world   ".strip  #=> "hello, world"
+
+
+# Trick ways to modify the strings
+
+"he77o".sub("7", "l")           #=> "hel7o"
+
+"he77o".gsub("7", "l")          #=> "hello"
+
+"hello".insert(-1, " dude")     #=> "hello dude"
+
+"hello world".delete("l")       #=> "heo word"
+
+"!".prepend("hello, ", "world") #=> "hello, world!"
